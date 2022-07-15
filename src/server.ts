2a172cfbe9a461106a9557797c3ebfcc084e0583
Gabriel Frasson <bielfc27@gmail.com>
2022-07-15 13:57:03 -0300

    Database connection

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
 import { app } from './app';
+import createConnection from './database';
+
+(async () => await createConnection())();
 
 app.listen(3333, () => { console.log('Server is running') });
